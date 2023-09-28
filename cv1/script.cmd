ad0da13505a51ced3a6142e3b835bbad30245ece
Chladas <chladjan02@gmail.com>
2023-09-28 13:50:49 +0200

    Tutorial 1 - Basic cmd scripts

--- cv1/script.cmd
+++ cv1/script.cmd
@@ -1,34 +1,33 @@
-echo off
+@echo off
 if not exist stats.txt echo 0 0 0 > stats.txt
 
-REM if we use cycle in script % -> %% 
+REM Using a loop in the script, replace % with %% 
 for /f "tokens=1,2,3" %%i in (stats.txt) do (
-    set celkem=%%i
-    set chyb=%%j
-    set chybvrade=%%k
+    set total=%%i
+    set errors=%%j
+    set errorsInRow=%%k
 )
 
 set "test_ip=1.1.2.5"
-set "threshhold=2"
+set "threshold=2"
 
 ping -n 1 %test_ip% > nul
 
 if errorlevel 1 (
     set RESULT=Fail
-    set /a chyb+=1
-    set /a chybvrade+=1
+    set /a errors+=1
+    set /a errorsInRow+=1
 ) else (
     set RESULT=OK
-    set chybvrade=0
+    set errorsInRow=0
 )
 
-set /a celkem+=1
+set /a total+=1
 echo %date%;%time%;%RESULT% >> log.txt
-echo %celkem% %chyb% %chybvrade% > stats.txt
+echo %total% %errors% %errorsInRow% > stats.txt
 
-REM HKCU - kam(registry current usera), /v pojmenovani dat co ukladame, /t datatype - REG_SZ = string, /d samotna data, /f force update 
-reg add HKCU\cviko /v nazev /t REG_SZ /d tohlejsoudata /f
-REM dotaz na tento register parametry - (kde hledam, co hledam)
-reg query HKCU\cviko /v nazev
+REM Registry update - HKCU (registry current user), /v value_name, /t data_type - REG_SZ = string, /d data, /f force update 
+reg add HKCU\cviko /v name /t REG_SZ /d sample_data /f
 
-if %chybvrade% EQU %threshhold% echo "Hodne chyb, styd se"
+REM Query this registry entry - (where to look, what to look for)
+reg query HKCU\cviko /v name
